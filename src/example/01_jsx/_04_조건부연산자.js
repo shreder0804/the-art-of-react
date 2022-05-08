f622e71a90f3cff15b5c58cf55079aadc39fc304
shreder0804 <shreder0804@gmail.com>
2022-05-08 21:10:09 +0900

    fix : 코드 추가

--- src/example/01_jsx/_04_조건부연산자.js
+++ src/example/01_jsx/_04_조건부연산자.js
@@ -3,6 +3,7 @@ import React from 'react';
 export default function _04_조건부연산자() {
   const name01 = '황재성';
   const name02 = '빵재성';
+  const name03 = undefined;
   return (
     <div>
       {/* 리액트에서는 if 대신 조건부연산자(삼항연산자)를 사용한다 */}
@@ -11,6 +12,10 @@ export default function _04_조건부연산자() {
       {/* 아래의 두 코드는 같은 기능이다 */}
       {name02 === '황재성' ? <h1>황재성입니다</h1> : null}
       {name02 === '황재성' && <h1>황재성입니다</h1>}
+      {/* undefined이면 아무 것도 나타나지 않는다 */}
+      {name03}
+      {/* undefined일 때 보여주고 싶은 문구가 있다면 || 연산자를 사용한다 */}
+      {name03 || <h1>undefined입니다</h1>}
     </div>
   );
 }
