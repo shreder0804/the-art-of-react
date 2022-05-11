8de6b6f4e6c458982c61d38af2693a26ba9a07a9
shreder0804 <shreder0804@gmail.com>
2022-05-11 13:01:12 +0900

    Fix : export default 구문 변경

--- src/example/02_component/_01_클래스형컴포넌트.js
+++ src/example/02_component/_01_클래스형컴포넌트.js
@@ -1,7 +1,7 @@
 // 클래스형 컴포넌트 선언방법
 import React, { Component } from "react";
 
-class _01_클래스형컴포넌트 extends Component {
+export default class _01_클래스형컴포넌트 extends Component {
   render() {
     const name = "황재성";
     return (
@@ -12,5 +12,3 @@ class _01_클래스형컴포넌트 extends Component {
     );
   }
 }
-
-export default _01_클래스형컴포넌트;
